3a6a5a5fa871be5e712d7bc6eb62ffff2198990a
Lonna Barlow <lonna@lemonblock.com>
2022-02-24 23:15:09 -0700

    server file fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"))
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 
 })
 
